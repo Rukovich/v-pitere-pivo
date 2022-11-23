15f8eccd0aed7fa378204e879220becf1e72a7de
Stas <Rukovich97@gmail.com>
2022-11-23 19:45:43 +0300

    finish

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 
 import '/style.css';
 import '/src/css/reset.css'; //Сброс стилей
+import '/src/css/aos.css'; //aos анимации
 import '/src/fonts/fonts.css'; //шрифты
 
 import '/src/scss/style.scss'; 
 import '/src/js/header.js'; 
 import '/src/js/tabs.js'; 
 import '/src/js/tabClick.js'; 
+import '/src/js/aosAnimated.js'; //aos анимации
+
